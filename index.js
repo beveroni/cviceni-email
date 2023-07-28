5998744de8544829cf632c74717f2e707bab5d53
Veronika Bendova <svechovaver@gmail.com>
2023-07-28 21:38:06 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju');
+const goodbye = (jmeno) => {
+  const emailClosingElm = document.querySelector('.email__closing');
+  emailClosingElm.textContent = `Na shledanou ${jmeno}`;
+};
+// goodbye('Pavel Ovesný');
+
+const fillSubject = (predmet) => {
+  document.querySelector('.email__subject').textContent = predmet;
+};
+
+const fillBody = (body, jmeno) => {
+  const bodyElm = document.querySelector('.email__body');
+  bodyElm.innerHTML = body;
+  const closingElm = document.querySelector('.email__closing');
+  closingElm.textContent = goodbye(jmeno);
+};
